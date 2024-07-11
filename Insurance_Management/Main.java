5ab52fa69ae4317a5f7400338353597c2d023c2b
keerthivasan0108 <91474234+keerthivasan0108@users.noreply.github.com>
2024-07-11 22:23:04 +0530

    Add files via upload

---
+++ Insurance_Management/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("==================================================");
+		System.out.println("\tWelcome To Life Insurance Policy");
+		System.out.println("==================================================");
+		Signup obj = new Signup();
+		
+
+	}
+}
